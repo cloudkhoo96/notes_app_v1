8efad0dbf37b849dd00c1a1426649794f93652d3
cloud7535@gmail.com <cloud7535@gmail.com>
2023-01-18 02:18:58 +0800

    first

---
+++ src/component/NoteForm.tsx
@@ -0,0 +1,115 @@
+import { FormEvent, useRef, useState } from "react";
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
+<<<<<<< HEAD
+import { Link } from "react-router-dom";
+=======
+import { Link, useNavigate } from "react-router-dom";
+>>>>>>> c851008 (first)
+import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "../App";
+import { v4 as uuidV4 } from "uuid";
+
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+<<<<<<< HEAD
+=======
+  const navigate = useNavigate();
+>>>>>>> c851008 (first)
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: selectedTags,
+    });
+<<<<<<< HEAD
+=======
+
+    navigate("..");
+>>>>>>> c851008 (first)
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control ref={titleRef} required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect
+<<<<<<< HEAD
+              onCreateOption={label => {
+                const newTag = { id: uuidV4(), label }
+                onAddTag(newTag)
+                setSelectedTags(prev => [...prev, newTag])
+              }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map(tag => {
+                    return { label: tag.label, value: tag.id }
+=======
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+>>>>>>> c851008 (first)
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control
+            required
+            as="textarea"
+            ref={markdownRef}
+            rows={15}
+            className="justify-content-end"
+          />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2}>
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+}
